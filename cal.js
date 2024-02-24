321fd3bba249ceddf48efb2089bfa066210e5725
hira baqar <148424134+hirabaqar@users.noreply.github.com>
2024-02-25 00:59:41 +0500

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+console.log("welcome to 'Making Simple Calculator' Develope by HIRA BAQAR  ");
+let calculation = await inquirer.prompt([
+    { type: "number",
+        name: "num1",
+        message: "Enter your first number?"
+    },
+    { type: "number",
+        name: "num2",
+        message: "Enter your second number?"
+    },
+    { type: "list",
+        name: "operator",
+        message: "select your operation?",
+        choices: ["sum", "sub", "multiply", "divide"]
+    }
+]);
+if (calculation.operator === "sum") {
+    console.log(`${calculation.num1} + ${calculation.num2} = ${calculation.num1 + calculation.num2}`);
+}
+else if (calculation.operator === "sub") {
+    console.log(`${calculation.num1} - ${calculation.num2} = ${calculation.num1 - calculation.num2}`);
+}
+else if (calculation.operator === "multiply") {
+    console.log(`${calculation.num1} * ${calculation.num2} = ${calculation.num1 * calculation.num2}`);
+}
+else if (calculation.operator === "divide") {
+    console.log(`${calculation.num1} / ${calculation.num2} = ${calculation.num1 / calculation.num2}`);
+}
+// expot{};
